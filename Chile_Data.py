ecca39e8e5b5ad9cfcbe70010fd2e4a930757899
csrak <cristiane.r11@gmail.com>
2019-12-27 00:53:42 +0100

    Scrapers for list of stocks and fillings ready W.I.P processing to obtain tickers

--- Chile_Data.py
+++ Chile_Data.py
@@ -11,7 +11,7 @@ month='03'
 year='2019'
 
 
-#Download function list not working/not used yet
+#Download list not working/not used yet
 def download_list(month,year):
     # www.cmfchile.cl/institucional/mercados/novedades_envio_sa_ifrs_excel2.php?aa=2019&mm=03
     template = 'http://www.cmfchile.cl/institucional/mercados/novedades_envio_sa_ifrs_excel2.php?'
@@ -26,14 +26,24 @@ def download_list(month,year):
     datafold='\\Data\\Chile\\'
     open(wd+datafold+file_name, 'wb').write(myfile.content)
 
+#Selects right url depending on scraping to do
+def url_generator(site,month,year):
+    url='no url found'
+    if site=='cmf'or site=='cmf.cl':
+        #http://www.cmfchile.cl/institucional/mercados/novedades_envio_sa_ifrs.php?mm_ifrs=03&aa_ifrs=2019 For first 3 months of 2019
+        template='http://www.cmfchile.cl/institucional/mercados/novedades_envio_sa_ifrs.php?mm_ifrs=' #For 2019
+
+        year=year
+        url=template+month+'&aa_ifrs='+year #Create a handle, page, to handle the contents of the website
+    elif site=='mwcl'or site=='MWCL' or site=='MarketWatchcl' or site =='Market Watch Chile':
+        url='https://www.marketwatch.com/tools/markets/stocks/country/chile'
+    else:
+        return url
+    return url
 
 
-def scrape_list(month,year):
-    #http://www.cmfchile.cl/institucional/mercados/novedades_envio_sa_ifrs.php?mm_ifrs=03&aa_ifrs=2019 For first 3 months of 2019
-    template='http://www.cmfchile.cl/institucional/mercados/novedades_envio_sa_ifrs.php?mm_ifrs=' #For 2019
-
-    year=year
-    url=template+month+'&aa_ifrs='+year #Create a handle, page, to handle the contents of the website
+#Create pandas DataFrame from list in an html webpage
+def scrape_lists(url):
     agent = {"User-Agent":'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
     page = requests.get(url, headers=agent)
     doc = lh.fromstring(page.content)#Parse data that are stored between <tr>..</tr> of HTML
@@ -77,24 +87,15 @@ def scrape_list(month,year):
     #print([len(C) for (title,C) in col])
     Dict={title:column for (title,column) in col}
     df=pd.DataFrame(Dict)
-    
-    df = df.drop("Fecha Primer envío", axis=1)
-    df = df.drop("Fecha último envío", axis=1)
-    df = df.drop("Tipo Balance", axis=1)
-    print(df.head())
-    wd=os.getcwd()
-    #folder=Path(wd).parent
-    #print(folder)
-    file_name='registered_stocks_'+month+'-'+year+'.csv'
-    datafold=wd+'\\Data\\Chile\\'
-    export_csv = df.to_csv(datafold+file_name, index = None, header=True)
     return df
- 
+#########################################################
+#########################################################
+######################################################### 
 
 # 
-# 
+#Scrap fillings obtains the fillings from a provided list in zip format (XBRL inside)
 #
-def scrap_filling(companies_list,n,month, year):
+def scrap_fillings(companies_list,n,month, year):
         #Download 
        # http://www.cmfchile.cl/institucional/inc/inf_financiera/ifrs/safec_ifrs_verarchivo.php?auth=&send=&
        # rut=77750920&mm=03&aa=2019&archivo=77750920_201903_I.zip&desc_archivo=Estados%20financieros%20(XBRL)&tipo_archivo=XBRL
@@ -113,8 +114,16 @@ def scrap_filling(companies_list,n,month, year):
         file_name=rut+'_'+month+year+'.zip'
         datafold='\\Data\\Chile\\'
         open(wd+datafold+file_name, 'wb').write(myfile.content)
+#########################################################
+#########################################################
+#########################################################
 
 
+
+#
+#Function read_data  rescues previously saved data from csv
+#Data is saved as a pandas DataFrame and returned
+#
 
 def read_data(filename,datafold):
     wd=os.getcwd()
@@ -127,11 +136,73 @@ def read_data(filename,datafold):
     return df
 
 
-file_name='registered_stocks_'+month+'-'+year+'.csv'
-datafold='\\Data\\Chile\\'
-df=read_data(file_name,datafold)
-print(df.head())
-print(df.shape[1])
+#scrap_rutlists obtains lsit of profiles of companies who have filled recently
+def scrap_rutlist(month,year):
+    url=url_generator('cmf',month,year)
+    df=scrape_lists('','',url)
+    df = df.drop("Fecha Primer envío", axis=1)
+    df = df.drop("Fecha último envío", axis=1)
+    df = df.drop("Tipo Balance", axis=1)
+    print(df.head())
+    wd=os.getcwd()
+    #folder=Path(wd).parent
+    #print(folder)
+    file_name='registered_stocks_'+month+'-'+year+'.csv'
+    datafold=wd+'\\Data\\Chile\\'
+    export_csv = df.to_csv(datafold+file_name, index = None, header=True)
+
+
+#scrap_MW obtains list of companies in the Santiago market
+def scrap_mw():
+    url=url_generator('mwcl','0','0')
+    df=scrape_lists(url)
+    df = df.drop("Exchange", axis=1)
+    print(df.head())
+    wd=os.getcwd()
+    #folder=Path(wd).parent
+    #print(folder)
+    file_name='registered_stocks_mw.csv'
+    datafold=wd+'\\Data\\Chile\\'
+    export_csv = df.to_csv(datafold+file_name, index = None, header=True)
+
+
+
+
+#
+#Function Get_Ruts obtains the rut from a provided list
+#Formatting is done to insert into url for scraping fillings
+#
+
+def get_ruts(month, year,file_name, datafold):
+    #file_name='registered_stocks_'+month+'-'+year+'.csv'
+    #datafold='\\Data\\Chile\\'
+    df=read_data(file_name,datafold)
+    df = df.astype(str)
+
+    #We take out the verifier code of each rut since it is not used
+    for i in range (0,df.shape[0]):
+        df.loc[i, 'Rut']=(df.loc[i, 'Rut'])[:-2]      
+    return df.loc[:, 'Rut']
+
+#########################################################
+#########################################################
+#########################################################
+
+scrap_mw()
+
+
+
+
+
+#
+#file_name='registered_stocks_'+month+'-'+year+'.csv'
+#datafold='\\Data\\Chile\\'
+#ruts=get_ruts(month,year,file_name,datafold)
+#print(ruts)
+
+
+#print(df.head())
+#print(df.shape[1])
 
 #df=scrape_list(month,year)
-#scrap_filling(df,n,month, year)
+#scrap_fillings(df,n,month, year)
