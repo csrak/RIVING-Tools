b84ea799fe95768ae42ed03657fd38a2835d3f02
csrak <cristiane.r11@gmail.com>
2019-12-26 19:00:34 +0100

    Functions for reading scraped data ready W.I.P: Functions to read fillings selected from the list

--- Chile_Data.py
+++ Chile_Data.py
@@ -28,12 +28,12 @@ def download_list(month,year):
 
 
 
-def scrape_list(month,year): # not ready
+def scrape_list(month,year):
     #http://www.cmfchile.cl/institucional/mercados/novedades_envio_sa_ifrs.php?mm_ifrs=03&aa_ifrs=2019 For first 3 months of 2019
     template='http://www.cmfchile.cl/institucional/mercados/novedades_envio_sa_ifrs.php?mm_ifrs=' #For 2019
 
-    year='&aa_ifrs='+year
-    url=template+month+year #Create a handle, page, to handle the contents of the website
+    year=year
+    url=template+month+'&aa_ifrs='+year #Create a handle, page, to handle the contents of the website
     agent = {"User-Agent":'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
     page = requests.get(url, headers=agent)
     doc = lh.fromstring(page.content)#Parse data that are stored between <tr>..</tr> of HTML
@@ -81,11 +81,57 @@ def scrape_list(month,year): # not ready
     df = df.drop("Fecha Primer envío", axis=1)
     df = df.drop("Fecha último envío", axis=1)
     df = df.drop("Tipo Balance", axis=1)
-    #print(df.head())
+    print(df.head())
     wd=os.getcwd()
     #folder=Path(wd).parent
     #print(folder)
     file_name='registered_stocks_'+month+'-'+year+'.csv'
     datafold=wd+'\\Data\\Chile\\'
     export_csv = df.to_csv(datafold+file_name, index = None, header=True)
-scrape_list(month,year)    
+    return df
+ 
+
+# 
+# 
+#
+def scrap_filling(companies_list,n,month, year):
+        #Download 
+       # http://www.cmfchile.cl/institucional/inc/inf_financiera/ifrs/safec_ifrs_verarchivo.php?auth=&send=&
+       # rut=77750920&mm=03&aa=2019&archivo=77750920_201903_I.zip&desc_archivo=Estados%20financieros%20(XBRL)&tipo_archivo=XBRL
+    template = 'http://www.cmfchile.cl/institucional/inc/inf_financiera/ifrs/safec_ifrs_verarchivo.php?auth=&send=&'
+    for  i in n:
+        rut=companies_list[i,2]  ##2 is the column of ruts
+
+        url=template+'rut='+rut+'&mm='+month+'&aa='+year+'&archivo='+rut+'_'+year+month+'_I.zip&desc_archivo=Estados%20financieros%20(XBRL)&tipo_archivo=XBRL'
+
+        agent = {"User-Agent":'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
+        myfile = requests.get(url, headers=agent)
+
+        wd=os.getcwd()
+        #folder=Path(wd).parent
+        #print(folder)
+        file_name=rut+'_'+month+year+'.zip'
+        datafold='\\Data\\Chile\\'
+        open(wd+datafold+file_name, 'wb').write(myfile.content)
+
+
+
+def read_data(filename,datafold):
+    wd=os.getcwd()
+    if os.path.exists(wd+datafold+filename):
+        df=pd.read_csv(wd+datafold+filename, header=0)
+    else:
+        print('File not found in '+ wd+datafold+filename)
+        return 0
+    
+    return df
+
+
+file_name='registered_stocks_'+month+'-'+year+'.csv'
+datafold='\\Data\\Chile\\'
+df=read_data(file_name,datafold)
+print(df.head())
+print(df.shape[1])
+
+#df=scrape_list(month,year)
+#scrap_filling(df,n,month, year)
