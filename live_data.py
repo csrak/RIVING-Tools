6dc57ded7ca015397cf5d268cfade76885fd051a
cristiane.r11@gmail.com <cristiane.r11@gmail.com>
2020-04-25 02:50:05 +0200

    Added MW scrapper (recommended instead of yahoo)
    Signed-off-by: cristiane.r11@gmail.com <cristiane.r11@gmail.com>

--- live_data.py
+++ live_data.py
@@ -14,13 +14,29 @@ import Chile_Data as CL
 from zipfile import ZipFile
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def mw_quote_CL(ticker): #Obtain quote from marketwatch finance
+    ticker=ticker
+    agent = {"User-Agent":'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
+    url = 'https://www.marketwatch.com/investing/stock/' + ticker+'?countrycode=cl'
+    response = requests.get(url, headers=agent,verify=False)
+    print ("Parsing %s"%(url))
+    parser = lh.fromstring(response.text)
+    summary_table = parser.xpath('//span[contains(@class,"value")]//text()')
+    if not summary_table:
+        quote=yahoo_quote_CL(ticker)
+    else:
+        quote=float(summary_table[0].replace(',',''))     
+    return quote
+
+
+
 def yahoo_quote_CL(ticker): #Obtain quote from yahoo finance
     ticker2=ticker+'.SN'
     agent = {"User-Agent":'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
     url = "http://finance.yahoo.com/quote/%s?p=%s"%(ticker2,ticker2)
     response = requests.get(url, headers=agent,verify=False)
     print ("Parsing %s"%(url))
-    sleep(4)
     parser = lh.fromstring(response.text)
     summary_table = parser.xpath('//span[contains(@class,"Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)")]//text()')
     if not summary_table:
@@ -29,18 +45,20 @@ def yahoo_quote_CL(ticker): #Obtain quote from yahoo finance
         quote=float(summary_table[0].replace(',',''))
     return quote
 def yahoo_quoteA_CL(ticker): #Obtain quote from yahoo finance
-    ticker=ticker+'-A.SN'
+    ticker2=ticker+'-A.SN'
     agent = {"User-Agent":'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
-    url = "http://finance.yahoo.com/quote/%s?p=%s"%(ticker,ticker)
+    url = "http://finance.yahoo.com/quote/%s?p=%s"%(ticker2,ticker2)
     response = requests.get(url, headers=agent,verify=False)
     print ("Parsing %s"%(url))
-    sleep(4)
     parser = lh.fromstring(response.text)
     summary_table = parser.xpath('//span[contains(@class,"Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)")]//text()')   
     if not summary_table:
-        quote='please create bloomberg or marketwatch scraper'
+        quote='Try another scraper'
     else:
         quote=float(summary_table[0].replace(',',''))     
     return quote
 
+
+
+#print(mw_quote_CL('SMU'))    
 #print(yahoo_quote_CL('SMU'))
